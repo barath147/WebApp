14ad3e8d36e190f9580402e5fdc37efb9c530dd8
barath147 <62895159+barath147@users.noreply.github.com>
2020-04-07 18:41:20 -0400

    case study QA ip address

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.217.119.242:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.21.154.146:5432/postgres";
 			String username="postgres";
 			String password="";
 		
